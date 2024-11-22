dafc4aa9219dd227046730b13ba31f979679f2ef
mario-willian <mario_willian@outlook.com>
2024-11-22 16:31:12 -0300

    Ajuste do codigo do webhook

--- webhook/Ultimo_Tempo_Lead.php
+++ webhook/Ultimo_Tempo_Lead.php
@@ -3,12 +3,16 @@ header('Content-Type: application/json');
 
 // Obter a data atual
 $today = date("Y-m-d");
+$current_time = date("H:i:s"); // Hora atual
+$timestamp = time(); // Timestamp Unix (inteiro)
 
 // Retornar a resposta em JSON
 echo json_encode([
     "success" => true,
     "data" => [
-        "dataAtual" => $today
+        "dataAtual" => $today,
+        "horaAtual" => $current_time,
+        "timestamp" => $timestamp // Valor inteiro para comparações
     ]
 ]);
 ?>
